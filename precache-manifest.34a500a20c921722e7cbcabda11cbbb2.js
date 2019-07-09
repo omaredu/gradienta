7b6e14ea0663265755e42575269476407c552ce0
Omaredu <omaredu.02@gmail.com>
2019-07-08 22:47:15 -0500

    Updates

---
+++ precache-manifest.34a500a20c921722e7cbcabda11cbbb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2308e11d892aa98e5f435672fbf21e7e",
+    "url": "/gradienta/index.html"
+  },
+  {
+    "revision": "b0f14d8816f55d02d4ed",
+    "url": "/gradienta/static/css/main.7b40a7be.chunk.css"
+  },
+  {
+    "revision": "626fb2a46e0dca036cb6",
+    "url": "/gradienta/static/js/2.9cf8da5f.chunk.js"
+  },
+  {
+    "revision": "b0f14d8816f55d02d4ed",
+    "url": "/gradienta/static/js/main.2ca5c79f.chunk.js"
+  },
+  {
+    "revision": "a192ab115d20686021eb",
+    "url": "/gradienta/static/js/runtime~main.dc5b2777.js"
+  },
+  {
+    "revision": "6fadab2c16e2a2bdb79c1dc546834ff5",
+    "url": "/gradienta/static/media/favicon.6fadab2c.ico"
+  },
+  {
+    "revision": "7dd24b5d5850859eb2bf5b59f9e09a69",
+    "url": "/gradienta/static/media/icon.7dd24b5d.svg"
+  }
+]);
